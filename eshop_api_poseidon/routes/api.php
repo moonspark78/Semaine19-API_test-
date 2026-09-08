26b2571679a6dd127333662cc39b124a3caa242a
Omar Jabour <jabour.omar@hotmail.com>
2026-09-08 14:55:22 +0200

    :package: TP2 :  order ressource  api   :card_file_box:

--- eshop_api_poseidon/routes/api.php
+++ eshop_api_poseidon/routes/api.php
@@ -4,58 +4,139 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/test', function() {
+
+
+Route::get('/test', function () {
     return [
         "message" => "Bienvenue sur l'API eshop Poseidon ! :)",
     ];
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
-
-
-
-
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{category}', [CategoryController::class, 'show']);
-
-Route::get('/test-response', function(){
+Route::get('/test-response', function () {
     return response()->json([
         'message' => 'Bonjour depuis l\'API'
     ], 201);
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
-// Route::get('/profile', function (Request $request){
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::middleware('auth:sanctum')->group(function() {
+Route::get('/products/{product}', [
+    ProductController::class,
+    'show'
+]);
 
-Route::get('/profile', function (Request $request){return $request->user();});
-Route::get('/orders', [OrderController::class, 'index']);
-Route::get('/orders/{order}', [OrderController::class, 'show']);
-Route::delete('/logout', [AuthController::class, 'logout']);
+
+
+
+Route::get('/categories', [
+    CategoryController::class,
+    'index'
+]);
+
+Route::get('/categories/{category}', [
+    CategoryController::class,
+    'show'
+]);
+
+
+
+
+Route::post('/register', [
+    AuthController::class,
+    'register'
+]);
+
+Route::post('/login', [
+    AuthController::class,
+    'login'
+]);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+
+    Route::get('/profile', function (Request $request) {
+        return $request->user();
+    });
+
+
+ 
+
+    // Voir ses propres commandes
+    Route::get('/orders', [
+        OrderController::class,
+        'index'
+    ]);
+
+
+    // Voir une de ses commandes
+    Route::get('/orders/{order}', [
+        OrderController::class,
+        'show'
+    ]);
+
+
+    // Créer une commande
+    Route::post('/orders', [
+        OrderController::class,
+        'store'
+    ]);
+
+
+ 
+
+    Route::delete('/logout', [
+        AuthController::class,
+        'logout'
+    ]);
 
 });
 
-Route::middleware(['auth:sanctum', 'admin'])->group(function() {
-    Route::post('/products', [ProductController::class, 'store']);
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    'admin'
+])->group(function () {
+
+    // Créer un produit
+    Route::post('/products', [
+        ProductController::class,
+        'store'
+    ]);
+
+
+    // Voir toutes les commandes
+    Route::get('/admin/orders', [
+        OrderController::class,
+        'adminIndex'
+    ]);
+
+
+    // Modifier le statut d'une commande
+    Route::patch('/admin/orders/{order}', [
+        OrderController::class,
+        'updateStatus'
+    ]);
+
 });
+
+
+
 
 Route::get('/limit', function () {
     return [
         "message" => "Je suis limité ?",
     ];
 })->middleware('throttle:api');
-
