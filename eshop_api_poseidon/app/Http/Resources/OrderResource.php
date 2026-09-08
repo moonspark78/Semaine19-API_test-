df26136a17281e1630c2ca4b909e7c9ce3b91edd
Omar Jabour <jabour.omar@hotmail.com>
2026-09-08 14:45:39 +0200

    :package: TP2 :  order ressource    :card_file_box:

---
+++ eshop_api_poseidon/app/Http/Resources/OrderResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+
+            'status' => $this->status,
+
+            'total' => $this->total,
+
+            'products' => $this->whenLoaded('products', function () {
+                return $this->products->map(function ($product) {
+                    return [
+                        'id' => $product->id,
+                        'name' => $product->name,
+                        'quantity' => $product->pivot->quantity,
+                        'price' => $product->pivot->price,
+                    ];
+                });
+            }),
+
+            'created_at' => $this->created_at,
+        ];
+    }
+}
