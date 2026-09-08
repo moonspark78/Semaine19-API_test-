77fbe1f904860c359a550c7f7e39c048e06a04e7
Omar Jabour <jabour.omar@hotmail.com>
2026-09-08 14:34:05 +0200

    :package: TP2 :  migration   ./  :card_file_box:

--- eshop_api_poseidon/database/migrations/2026_09_08_123030_create_order_product_table.php
+++ eshop_api_poseidon/database/migrations/2026_09_08_123030_create_order_product_table.php
@@ -6,20 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('order_product', function (Blueprint $table) {
+
             $table->id();
+
+            $table->foreignId('order_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('product_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->unsignedInteger('quantity');
+
+            $table->decimal('price', 10, 2);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('order_product');
