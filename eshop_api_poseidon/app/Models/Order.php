b8a6f09b4581a1bfbe474de7b2518efe275a66cd
Omar Jabour <jabour.omar@hotmail.com>
2026-09-08 14:42:25 +0200

    :package: TP2 :  migration   ./  :card_file_box:

--- eshop_api_poseidon/app/Models/Order.php
+++ eshop_api_poseidon/app/Models/Order.php
@@ -6,8 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'status',
+        'total',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)
+            ->withPivot('quantity', 'price')
+            ->withTimestamps();
     }
 }
